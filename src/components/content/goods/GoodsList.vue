c3b93fc339171c453df171f480aa5e003b62745a
lishuai <lsdark_cl@foxmail.com>
2020-05-24 17:57:37 +0800

    首页修改

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="goods">
+    <!--<p v-for="(item,index) in goodsList" :key="index" v-text="item"></p>-->
+  <goods-list-item v-for="(item,index) in goodsList" :key="index" :goods-item="item"></goods-list-item>
+
+</div>
+</template>
+
+<script>
+  /*导入GoodsListItem*/
+  import  GoodsListItem from './GoodsListItem'
+    export default {
+        name: "GoodsList",
+      props:{
+        goodsList:{
+          type:Array,
+          default(){
+            return[]
+          }
+        }
+      },
+      components:{
+        GoodsListItem
+      }
+    }
+</script>
+
+<style scoped>
+  .goods {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+
+    padding: 2px;
+  }
+</style>
--- src/network/home.js
+++ src/network/home.js
@@ -6,3 +6,12 @@ export function getHomeData() {
     url: '/home/multidata'
   })
 }
+export function getHomeGoods(type, page) {
+  return request({
+    url: '/home/data',
+    params: {
+      type,
+      page
+    }
+  })
+}
