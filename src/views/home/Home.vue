e9f42eee23662aa655fb6ac2f2b9344b0686b17b
lishuai <lsdark_cl@foxmail.com>
2020-05-24 17:57:37 +0800

    首页修改对的tag
    Signed-off-by: lishuai <lsdark_cl@foxmail.com>

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,7 +3,7 @@
     <nav-bar class="home-nav">
       <p slot="left">返回</p>
       <p slot="center">购物商场</p>
-      <p slot="right">没有用</p>
+      <p slot="right">暂时无用的</p>
     </nav-bar>
 
     <!--  这里轮播的是banners的数据-->
@@ -13,119 +13,25 @@
     <!--这是本周流行热卖-->
     <feature-view></feature-view>
     <!--这个是流行，新款，精选标题-->
-    <tab-control :titles="['流行','新款','精选']"></tab-control>
-    <ul>
-      <li>列表1</li>
-      <li>列表2</li>
-      <li>列表3</li>
-      <li>列表4</li>
-      <li>列表5</li>
-      <li>列表6</li>
-      <li>列表7</li>
-      <li>列表8</li>
-      <li>列表9</li>
-      <li>列表10</li>
-      <li>列表11</li>
-      <li>列表12</li>
-      <li>列表13</li>
-      <li>列表14</li>
-      <li>列表15</li>
-      <li>列表16</li>
-      <li>列表17</li>
-      <li>列表18</li>
-      <li>列表19</li>
-      <li>列表20</li>
-      <li>列表21</li>
-      <li>列表22</li>
-      <li>列表23</li>
-      <li>列表24</li>
-      <li>列表25</li>
-      <li>列表26</li>
-      <li>列表27</li>
-      <li>列表28</li>
-      <li>列表29</li>
-      <li>列表30</li>
-      <li>列表31</li>
-      <li>列表32</li>
-      <li>列表33</li>
-      <li>列表34</li>
-      <li>列表35</li>
-      <li>列表36</li>
-      <li>列表37</li>
-      <li>列表38</li>
-      <li>列表39</li>
-      <li>列表40</li>
-      <li>列表41</li>
-      <li>列表42</li>
-      <li>列表43</li>
-      <li>列表44</li>
-      <li>列表45</li>
-      <li>列表46</li>
-      <li>列表47</li>
-      <li>列表48</li>
-      <li>列表49</li>
-      <li>列表50</li>
-      <li>列表51</li>
-      <li>列表52</li>
-      <li>列表53</li>
-      <li>列表54</li>
-      <li>列表55</li>
-      <li>列表56</li>
-      <li>列表57</li>
-      <li>列表58</li>
-      <li>列表59</li>
-      <li>列表60</li>
-      <li>列表61</li>
-      <li>列表62</li>
-      <li>列表63</li>
-      <li>列表64</li>
-      <li>列表65</li>
-      <li>列表66</li>
-      <li>列表67</li>
-      <li>列表68</li>
-      <li>列表69</li>
-      <li>列表70</li>
-      <li>列表71</li>
-      <li>列表72</li>
-      <li>列表73</li>
-      <li>列表74</li>
-      <li>列表75</li>
-      <li>列表76</li>
-      <li>列表77</li>
-      <li>列表78</li>
-      <li>列表79</li>
-      <li>列表80</li>
-      <li>列表81</li>
-      <li>列表82</li>
-      <li>列表83</li>
-      <li>列表84</li>
-      <li>列表85</li>
-      <li>列表86</li>
-      <li>列表87</li>
-      <li>列表88</li>
-      <li>列表89</li>
-      <li>列表90</li>
-      <li>列表91</li>
-      <li>列表92</li>
-      <li>列表93</li>
-      <li>列表94</li>
-      <li>列表95</li>
-      <li>列表96</li>
-      <li>列表97</li>
-      <li>列表98</li>
-      <li>列表99</li>
-      <li>列表100</li>
-    </ul>
+    <tab-control :titles="['流行','新款','精选']" @tabClick="tbs"></tab-control>
+    <goods-list :goodsList="showGoodsList"></goods-list>
   </div>
 </template>
 
 <script>
+  /*导航栏*/
   import NavBar from 'components/common/navbar/NavBar'
+  /*轮播组件*/
   import HomeSwiper from './childComps/HomeSwiper'
+  /*这是本周流行热卖*/
   import RecommendView from './childComps/RecommendView'
+  /*展示每周精选*/
   import FeatureView from './childComps/FeatureView'
+  /*主页分类滚动*/
   import TabControl from 'components/content/tabControl/TabControl'
-  import {getHomeData} from 'network/home'
+  /*商品展示*/
+  import  GoodsList from 'components/content/goods/GoodsList'
+  import {getHomeData, getHomeGoods} from 'network/home'
 
 
   export default {
@@ -135,7 +41,8 @@
       HomeSwiper,
       RecommendView,
       FeatureView,
-      TabControl
+      TabControl,
+      GoodsList
     },
     data() {
       return {
@@ -153,16 +60,60 @@
     /*在创建组件成功的时候，就需要马上初始化数据，所以应该使用生命周期的create*/
     created() {
       //1.请求多个数据
-      getHomeData().then(res => {
-        console.log(res);
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-      }).catch(err => {
-        console.log(err);
-      })
+
+     this.getHomeDatas();
+      //请求个性数据
+      this.getHomeGoodsDatas('pop');
+      this.getHomeGoodsDatas('new');
+      this.getHomeGoodsDatas('sell');
+
+    },
+    /*计算属性*/
+    computed:{
+      showGoodsList(){
+        return  this.goods[this.currentType].list
+      }
+    },
+    methods:{
+      getHomeDatas(){
+        getHomeData().then(res => {
+          // console.log(res);
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        }).catch(err => {
+          console.log(err);
+        })
+      },
+
+      //请求个性数据
+      getHomeGoodsDatas(type){
+        const page=this.goods[type].page+1;
+        getHomeGoods(type,page).then(res=>{
+          console.log(res);
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page+=1;
+        }).catch(err=>{
+          console.log(err);
+        })
+      },
+      tbs(index){
+        switch (index) {
+          case 0:
+            this.currentType = 'pop'
+            break
+          case 1:
+            this.currentType = 'new'
+            break
+          case 2:
+            this.currentType = 'sell'
+            break
+        }
+      }
+
     }
 
   }
+
 </script>
 
 <style scoped>
@@ -186,7 +137,7 @@
   .tab-control {
     position: sticky;
     top: 44px;
-    z-index: 9;
+    z-index: 10;
   }
 
   .content {
