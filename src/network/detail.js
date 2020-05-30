599117c033411cb5d19ff7395642eebba691a2c1
Administrator <igssis@hotmail.com>
2020-05-30 19:55:06 +0800

    项目更新001

--- src/network/detail.js
+++ src/network/detail.js
@@ -9,3 +9,16 @@ export function getDetail(iid) {
     }
   })
 }
+
+export class GoodsInfo {
+  constructor(itemInfo, columns, services) {
+    this.title = itemInfo.title;
+    this.desc = itemInfo.desc;
+    this.newPrice = itemInfo.price;
+    this.oldPrice = itemInfo.oldPrice;
+    this.discount = itemInfo.discountDesc;
+    this.columns = columns;
+    this.services = services;
+    this.relaPrice = itemInfo.lowNowPrice
+  }
+}
--- src/views/detail/GoodsDetail.vue
+++ src/views/detail/GoodsDetail.vue
@@ -1,63 +1,38 @@
 <template>
   <div id="details">
     <detail-bar></detail-bar>
-    <!--    <home-swiper :banners="topImages"></home-swiper>-->
     <detail-swiper :imgs="topImages"></detail-swiper>
+    <detail-base-info :goods="goods"></detail-base-info>
 
-    <!-- <div ><p v-for="item in result.columns" v-text="item"></p></div>
-     <div>
-       <p v-text="result.detailInfo.desc"></p>
-     <div v-for="item in  result.detailInfo.detailImage">
-       <p v-text="item.anchor"></p>
-       <p v-text="item.key"></p>
-       <p v-text="item.key"></p>
-       <div >
-         <img v-for='img in item.list' :src="img" style="width: 95% ;height: auto">
-       </div>
-     </div>
-     </div>-->
   </div>
 </template>
 
 <script>
-  import {getDetail} from "../../network/detail";
+  import {getDetail, GoodsInfo} from "../../network/detail";
   import DetailBar from "./childComponents/DetailBar";
   import DetailSwiper from "./childComponents/DetailSwiper";
+  import DetailBaseInfo from './childComponents/DetailBaseInfo'
 
   export default {
     name: "GoodsDetail",
     components: {
-      DetailBar, DetailSwiper
+      DetailBar, DetailSwiper, DetailBaseInfo
     },
     data() {
       return {
         id: this.$route.params.id,
         topImages: [],
-        result: {
-          columns: [],
-          detailInfo: {
-            desc: '',
-            detailImage: [
-              {
-                anchor: "model_img",
-                desc: "",
-                key: "",
-                list: []
-              }
-            ]
-          }
-        }
+        goods: {}
       }
     },
     methods: {},
     mounted() {
       getDetail(this.id).then(res => {
+        let datas = res.result
         /*获取顶部轮播数据*/
-        this.topImages = res.result.itemInfo.topImages
-        this.result = res.result;
-        console.log(res)
-        console.log('=====')
-        // console.log(this.topImages)
+        this.topImages = datas.itemInfo.topImages
+        /*获取商品详情*/
+        this.goods = new GoodsInfo(datas.itemInfo, datas.columns, datas.shopInfo.services)
       })
     },
     activated() {
